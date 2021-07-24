e192657f4e3c414d98f0e10b032cf27c5ee4632e
John <dawsonmcnally@outlook.com>
2021-07-24 16:26:49 +0100

    tailwind1

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,46 +1,6 @@
 import Header from "../header";
 import Footer from "../footer";
 
-import List from '/src/components/List.js';
-import withListLoading from '/src/components/withListLoading';
-
-function App() {
-  const ListLoading = withListLoading(List);
-  const [appState, setAppState] = useState({
-    loading: false,
-    repos: null,
-  });
-
-
-        
-  useEffect(() => {
-    setAppState({ loading: true });
-    const apiUrl = `https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/football-oyono/service/GetFootballResults/incoming_webhook/GetFootballResultsHooks`;
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((repos) => {
-        setAppState({ loading: false, repos: repos });
-      });
-  }, [setAppState]);
-  return (
-    
-    <div className='App'>
-     <Layout>
-      <div className='container'>
-        <h1>xxxOld premier league games </h1>
-      </div>
-      </Layout>
-      <div className='repo-container'>
-        <ListLoading isLoading={appState.loading} repos={appState.repos} />
-      </div>
-      
-        
-        </div>        
-        
-  );
-  
-}
-
 
 
 function Layout ({children}) {
