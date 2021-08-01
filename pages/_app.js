0eb63c797c6a00312bd69db5f4302d158ccb3294
John <dawsonmcnally@outlook.com>
2021-08-01 20:39:29 +0100

    another Menu1

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@
 import '../styles/style.scss'
 import '../styles/Dropdown.css';
 import '../styles/Navbar.css';
-import '../styles/button.css'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
