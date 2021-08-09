fe23559460317930509181404d188b72a651d65c
John <dawsonmcnally@outlook.com>
2021-08-09 23:11:51 +0100

    header updated for mobile1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,30 +6,32 @@ const Navigation = ({}) => {
   const [isExpanded, toggleExpansion] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-  <div className="flex items-center flex-shrink-0 text-white mr-6">
-    <h2>Kangu</h2>
+    
+    <nav className="flex items-center  justify-between flex-wrap p-6  bg-gray-100  ">
+  <div className="flex items-center flex-shrink-0 bg-red-600 text-white mr-6">
+    <h2>Planet Anfield</h2>
 
   </div>
   <div className="block lg:hidden">
     <button
-    className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+    className="flex items-center px-3 py-2 border rounded text-teal-200 bg-gray-300 border-teal-400 hover:text-white hover:border-white"
     onClick={() => toggleExpansion(!isExpanded)}
     >
-      <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
     </button>
   </div>
   <div className={`${
             isExpanded ? `block` : `hidden`
-          } w-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
+          } w-full block flex-grow lg:flex lg:items-center   lg:w-auto`}>
     <div className="text-sm lg:flex-grow">
-      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-red-600 hover:text-white mr-4">
         Docs
       </a>
-      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-red-600 hover:text-white mr-4">
         Examples
       </a>
-      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-red-600 hover:text-white">
         test
       </a>
 
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,43 +3,40 @@ import Link  from 'next/link';
 
 function Footer (){
     return(
-      <footer className="text-gray-600 body-font h-screen mb-auto">
-  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col h-screen mb-auto">
-    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span className="ml-3 text-xl">Planet Anfield</span>
-    </a>
-    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 POC v0.1 —
-      <a href="https://twitter.com/planetanfield" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@global_delay</a>
-    </p>
-    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a className="text-gray-500">
-        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-        </svg>
-      </a>
-      <a className="ml-3 text-gray-500">
-        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-        </svg>
-      </a>
-      <a className="ml-3 text-gray-500">
-        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-        </svg>
-      </a>
-      <a className="ml-3 text-gray-500">
-        <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
-          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-          <circle cx="4" cy="4" r="2" stroke="none"></circle>
-        </svg>
-      </a>
-    </span>
+     
+  <div className="bg-gray-300 text-white  p-8">
+   
+   <div className="mt-6 lg:mt-0 lg:flex-1">
+                    <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
+                        <div>
+                            <h3 className="text-gray-700 uppercase dark:text-white">About</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">community</a>
+                          </div>
+
+                        <div>
+                            <h3 className="text-gray-700 uppercase dark:text-white">Blog</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Tech</a>
+                           
+                        </div>
+
+                        <div>
+                            <h3 className="text-gray-700 uppercase dark:text-white">Products</h3>
+                            <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">AAAA</a>
+                                       </div>
+
+                        <div>
+                            <h3 className="text-gray-700 uppercase dark:text-white">Contact</h3>
+                           
+                            <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">example@email.com</span>
+                        </div>
+                    </div>
+                </div>
+
+     <div>
+        <p className="text-center text-gray-800 dark:text-white">© Brand 2020 - All rights reserved</p>
+     </div>
   </div>
-</footer>
+
    
 
     )
